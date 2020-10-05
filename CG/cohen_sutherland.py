d79866d73ab3e2e645ec23c6bcf2660f4ae89785
ZHANGKAIHENG <120181080602@ncepu.edu.cn>
2020-10-06 00:26:08 +0800

    finish the core of the algorithm

--- CG/cohen_sutherland.py
+++ CG/cohen_sutherland.py
@@ -12,9 +12,23 @@ class Point:
     def __init__(self, a, b):
         self.x = a
         self.y = b
+        self.pos_code = 0
 
     def draw(self):
         plt.plot([self.x], [self.y], 'o')
+
+    def set_pos_code(self, sec):
+        # sec为区间列表，下标为0-3的项分别为裁剪框xmin， xmax， ymin， ymax
+        # bin_pos_code为pos_code的二进制形式，按位倒序存储在列表中
+        bin_pos_code = []
+        bin_pos_code.append(1) if self.x < sec[0] else bin_pos_code.append(0)
+        bin_pos_code.append(1) if self.x > sec[1] else bin_pos_code.append(0)
+        bin_pos_code.append(1) if self.y < sec[2] else bin_pos_code.append(0)
+        bin_pos_code.append(1) if self.y > sec[3] else bin_pos_code.append(0)
+        print(bin_pos_code)
+        # 转化为十进制
+        for i in range(4):
+            self.pos_code += bin_pos_code[i] * (2 ** i)
 
     def __str__(self):
         point_info = '({}, {})'.format(self.x, self.y)
@@ -33,9 +47,20 @@ class Line:
         self.b = self.start.y - self.start.x * self.k \
             if self.k is not None \
             else None
+        # ymin：直线最小纵坐标 ymax：直线最大纵坐标
+        self.ymin = self.start.y if self.start.y < self.end.y else self.end.y
+        self.ymax = self.start.y if self.start.y > self.end.y else self.end.y
 
     def draw(self):
         plt.plot([self.start.x, self.end.x], [self.start.y, self.end.y], '--')
+
+    def get_node(self, other_line):
+        node_x = (self.k - other_line.k) / (other_line.b - self.b)
+        node_y = self.k * node_x + self.b
+        if self.ymin <= node_y <= self.ymax:
+            return Point(node_x, node_y)
+        else:
+            return None
 
     def __str__(self):
         if self.k is None:
@@ -52,14 +77,16 @@ class CuttingBox:
     line_list = []
 
     def __init__(self):
-        x1, y1 = map(int, input('请输入裁剪框其中一个顶点横、纵坐标（用空格隔开）：').split())
-        x2, y2 = map(int, input('请输入与输入顶点在同一对角线上的顶点横、纵坐标（用空格隔开）：').split())
-
+        x1, y1 = map(int, input('请输入裁剪框左上角顶点横、纵坐标（用空格隔开）：').split())
+        x2, y2 = map(int, input('请输入裁剪框右下角顶点横、纵坐标（用空格隔开）：').split())
+        self.section = [x1, x2, y2, y1]
+        print(self.section)
+        # 顶点顺序：左上，右上，右下，左下
         point1 = Point(x1, y1)
         point2 = Point(x2, y1)
         point3 = Point(x2, y2)
         point4 = Point(x1, y2)
-
+        # 边顺序：上右下左
         line1 = Line(point1, point2)
         line2 = Line(point2, point3)
         line3 = Line(point3, point4)
@@ -97,5 +124,11 @@ if __name__ == '__main__':
     cline.draw()
     cbox = CuttingBox()
     cbox.draw()
+    cline.start.set_pos_code(cbox.section)
+    print(cline.start)
+    print(cline.start.pos_code)
+    cline.end.set_pos_code(cbox.section)
+    print(cline.end)
+    print(cline.end.pos_code)
     plt.grid(color='grey')
     plt.show()
